4ec9c69b67c18f2cc6386d287b919fe122c74ad1
Oguz <oguzkopan@gmail.com>
2026-01-19 22:37:10 +0300

    Initial Commit

---
+++ Simon/SimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonApp.swift
+//  Simon
+//
+//  Created by Oguz Kopan on 19.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
